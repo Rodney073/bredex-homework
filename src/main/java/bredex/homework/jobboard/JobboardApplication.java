35e7902623feb75426e8343c093efc7043c54509
unknown <ferenczimonika91@gmail.com>
2022-07-01 18:17:11 +0200

    First commit. Add basic project structure

---
+++ src/main/java/bredex/homework/jobboard/JobboardApplication.java
@@ -0,0 +1,14 @@
+package bredex.homework.jobboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobboardApplication.class, args);
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ src/test/java/bredex/homework/jobboard/JobboardApplicationTests.java
@@ -0,0 +1,13 @@
+package bredex.homework.jobboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
